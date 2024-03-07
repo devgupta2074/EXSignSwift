babe7cd3344ce06fd466076bb0f9e81900fc0b88
Dev Gupta <73140257+dev265545@users.noreply.github.com>
2024-03-07 10:03:51 +0530

    adding some very basic structure

---
+++ ex-signswift/app/(dashboard)/user/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <main>
+      User Dashboard with ID here the user should be directed to after login
+      here will be the upload and table function
+    </main>
+  );
+}
