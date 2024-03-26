5a4f84202344cd6198a1e34c8c0d77513913becf
Dev Gupta <73140257+dev265545@users.noreply.github.com>
2024-03-26 15:04:21 +0530

    next config update

--- ex-signswift/next.config.mjs
+++ ex-signswift/next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    GOOGLE_CLIENT_ID:
+      "56946012782-4fos0hcshm0n4jrfs9h3e5ril1hma1up.apps.googleusercontent.com",
+    GOOLE_CLIENT_SECRET: "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",
+
+    NEXTAUTH_SECRET:
+      "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcxMDc1NDg0NywiaWF0IjoxNzEwNzU0ODQ3fQ.9n-x_Zw4syq4hv_IqtQE8S1KhxdNoRfwLUj7kA_Ublo",
+  },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     // config.module.rules.push({
@@ -20,15 +28,6 @@ const nextConfig = {
   },
   reactStrictMode: false,
 };
-const nextConfig = {
-  env: {
-    GOOGLE_CLIENT_ID:
-      "56946012782-4fos0hcshm0n4jrfs9h3e5ril1hma1up.apps.googleusercontent.com",
-    GOOLE_CLIENT_SECRET: "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",
 
-    NEXTAUTH_SECRET:
-      "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcxMDc1NDg0NywiaWF0IjoxNzEwNzU0ODQ3fQ.9n-x_Zw4syq4hv_IqtQE8S1KhxdNoRfwLUj7kA_Ublo",
-  },
-};
 
 export default nextConfig;
