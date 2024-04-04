eb1afeeb4f6a4010fadb2c5a8881401cf6b20b0c
arguptaex <architgupta.rke@gmail.com>
2024-04-04 09:06:44 +0530

    nodemailer email switch

--- ex-signswift/app/api/emailsent/route.tsx
+++ ex-signswift/app/api/emailsent/route.tsx
@@ -3,24 +3,71 @@ import { NextRequest, NextResponse } from "next/server";
 import EmailTemp from "../../emails/EmailTemp";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.EMAIL_KEY);
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
+dotenv.config();
+
+let transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: process.env.SMTP_MAIL, // generated ethereal user
+    pass: process.env.SMTP_PASSWORD, // generated ethereal password
+  },
+});
 
 export async function POST(req: NextRequest) {
-  // console.log(await req.json())
-  const { email, subject, email_body } = await req.json();
-  console.log(email);
-  try {
-    const data = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      // to:"ag213@snu.edu.in",
-      to: email,
-      subject: "EX Squared India: Full Time Job Letter",
-      react: EmailTemp({ subject, email_body }),
-    });
+  // const { email, subject, message } = req.body;
+  // console.log(email, subject, message);
+  // const welcomeContent = useServerAction(() => {
+  //     return <Welcome />;
+  // });
 
-    return NextResponse.json(data);
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({ error });
-  }
+  console.log("check");
+
+  //   const emailHtml = renderTo(<Welcome />);
+
+  var mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: ["ag213@snu.edu.in", "gupta.archit01@gmail.com"],
+    subject: "check_multiple in main",
+    // react: <Welcome />
+    // html: emailHtml,
+    html: " <h3>heloo</h3>",
+  };
+
+  console.log("check2");
+  await transporter.sendMail(mailOptions, function (error: any, info: any) {
+    if (error) {
+      // console.log(check3)
+      console.log(error);
+    } else {
+      console.log("Email sent successfully!");
+      return NextResponse.json("data");
+    }
+  });
+  return NextResponse.json("data");
 }
+
+// const resend = new Resend(process.env.EMAIL_KEY);
+
+// export async function POST(req: NextRequest) {
+//   // console.log(await req.json())
+//   const { email, subject, email_body } = await req.json();
+//   console.log(email);
+//   try {
+//     const data = await resend.emails.send({
+//       from: "onboarding@resend.dev",
+//       // to:"ag213@snu.edu.in",
+//       to: email,
+//       subject: "EX Squared India: Full Time Job Letter",
+//       react: EmailTemp({ subject, email_body }),
+//     });
+
+//     return NextResponse.json(data);
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json({ error });
+//   }
+// }
