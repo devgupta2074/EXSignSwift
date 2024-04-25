0c883201fd0d4d9e244d70af12b71bc2409e8c3a
ArchitGupta07 <gupta.archit01@gmail.com>
2024-04-25 21:27:46 +0530

    tablist css added

--- ex-signswift/components/TableMenu.tsx
+++ ex-signswift/components/TableMenu.tsx
@@ -1,24 +1,28 @@
 import React from "react";
+import { FaSignature } from "react-icons/fa6";
+import { FaRegClock } from "react-icons/fa";
+import { IoIosCheckmarkCircleOutline } from "react-icons/io";
+import { IoMdDocument } from "react-icons/io";
 
 const TableMenu = () => {
   return (
     <div
       role="tablist"
       aria-orientation="horizontal"
-      className="bg-muted gap-20 p-4 text-muted-foreground inline-flex h-10 items-center justify-center rounded-md p-1"
+      className="dashboard-tablist bg-muted  text-muted-foreground inline-flex h-10 items-center justify-center rounded-md p-1"
       data-orientation="horizontal"
     >
       <button className="text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900">
-        Inbox
+      <FaSignature className="react-icon"/>Inbox 
       </button>
       <button className="text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900">
-        Draft
+        <FaRegClock className="react-icon"/>Draft
       </button>
       <button className="text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900">
-        Pending
+        <IoIosCheckmarkCircleOutline className="react-icon"/>Pending
       </button>
       <button className="text-gray-600 hover:text-gray-900 focus:outline-none focus:text-gray-900">
-        All
+        <IoMdDocument className="react-icon" />All
       </button>
     </div>
   );
