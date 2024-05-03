aac20aa66c3ab1a2c541ec019b30be21405addd2
Dev Gupta <73140257+dev265545@users.noreply.github.com>
2024-05-03 12:36:54 +0530

    fixing code for build

--- ex-signswift/app/api/auth/[...nextauth]/route.ts
+++ ex-signswift/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 import { config } from "@/lib/auth";
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth/next";
 
-export const handler = NextAuth(config);
+const handler = NextAuth(config);
 
 export { handler as GET, handler as POST };
