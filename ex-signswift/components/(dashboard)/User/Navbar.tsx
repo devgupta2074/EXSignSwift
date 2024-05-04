5a13d9bfe42f113a03e5ba87f53a886fa0054667
Dev Gupta <73140257+dev265545@users.noreply.github.com>
2024-05-05 02:08:09 +0530

    commit

--- ex-signswift/components/(dashboard)/User/Navbar.tsx
+++ ex-signswift/components/(dashboard)/User/Navbar.tsx
@@ -1,32 +1,50 @@
 "use client";
 import * as React from "react";
-import { ComboboxDemo } from "./CombBox";
-import { Button } from "@/components/ui/button";
+
 import { redirect, useRouter } from "next/navigation";
 import H2 from "@/components/Typography/H2";
-import H4 from "@/components/Typography/H4";
 
+import logo1 from "../../../public/ex_logg.png";
 import Image from "next/image";
-import { FaPenNib } from "react-icons/fa";
-import { Link } from "lucide-react";
+// import { FaPenNib } from "react-icons/fa";
+// import { Link } from "lucide-react";
+// import {
+//   Select,
+//   SelectContent,
+//   SelectItem,
+//   SelectTrigger,
+// } from "@/components/ui/select";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Navbar = (props: any) => {
   const router = useRouter();
 
+  // redirect(`/api/auth/signout`)   wont't work because this is client component it would have worked in server side componenet
   function handleSignOut() {
     console.log("signout click");
     router.push("/api/auth/signout");
     // redirect(`/api/auth/signout`)   wont't work because this is client component it would have worked in server side componenet
   }
   return (
-    <nav className="nav-container">
-      <div className=" nav-container-two flex items-center ">
+    <nav className=" bg-white shadow-xl  fixed top-0 w-full z-50 ">
+      <div className="w-full bg-rose-100 h-10">
+        <div className="flex items-center justify-center p-1 text-sm font-medium font-sans ">
+          Solution Porvided by Ex Squared Solutions
+        </div>
+      </div>
+      <div className="px-12 py-3   nav-container-two flex items-center bg-white text-black">
         {/* <div className=" nav-container-three w-full flex flex-row justify-between  "> */}
-        <div className="nav-Logo-section w-1/2  gap-20">
+        <div className="nav-Logo-section  flex items-center w-1/2  gap-5 text-black">
+          <Image width={"30"} height={"30"} src={logo1} alt="logo" />
           <H2>
-            {" "}
-            <FaPenNib />
-            SignSwift
+            <div className="text-[#0F172A]">SignSwift</div>
           </H2>
           {/* <H4>Documents</H4> */}
         </div>
@@ -34,21 +52,53 @@ const Navbar = (props: any) => {
           {/* <Image className="profile-icon-image" src={logo} alt="logo" /> */}
           <a href="/">
             <Image
-              className="profile-icon-image rounded-full"
+              className="rounded-full"
               src={props.user.image}
-              width={25}
-              height={25}
+              width={40}
+              height={40}
               alt="logo"
             />
           </a>
           {/* <p>Profile Icon</p> */}
           <div className="user-data-logo">
-            <p>{props.user.name}</p>
-            <p>{props.user.email}</p>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="flex  gap-5  justify-start items-center flex-col text-sm">
+                <div className="flex flex-row gap-4">
+                  <div className="flex flex-col items-start justify-start">
+                    <p>{props.user.name}</p>
+                    <p>Personal Account</p>
+                  </div>
+                  <div className="flex flex-col justify-center">
+                    <p
+                      style={{
+                        marginBottom: "-8px",
+                        width: "20px",
+                        color: "#64748B",
+                      }}
+                    >
+                      ^
+                    </p>
+                    <p
+                      className="rotate-180"
+                      style={{
+                        marginTop: "-8px",
+                        width: "20px",
+                        color: "#64748B",
+                      }}
+                    >
+                      ^
+                    </p>
+                  </div>
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-48">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>SignOut</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
-          <button className="signout-btn" onClick={handleSignOut}>
-            Sign Out
-          </button>
+          <div></div>
         </div>
       </div>
       {/* </div> */}
--- ex-signswift/components/formstep2.tsx
+++ ex-signswift/components/formstep2.tsx
@@ -36,6 +36,7 @@ export default function Step2({
   const router = useRouter();
   const [name, setName] = useState<String[]>([]);
   const [email, setEmail] = useState<String[]>([]);
+  const [selectedValue, setSelectedValue] = useState<string>("");
 
   const {
     loading: loading2,
@@ -91,7 +92,12 @@ export default function Step2({
     updatedEmails.splice(index, 1, event.target.value.toLowerCase());
     setEmail(updatedEmails);
   };
-
+  const [emails, setEmails] = useState<any>([
+    { email: "", name: "signers." + "0" + ".email" },
+  ]);
+  const [names, setNames] = useState<any>([
+    { receptientname: "", name: "signers." + "0" + ".name" },
+  ]);
   const [inputs, setInputs] = useState<InputObject[]>([
     {
       id: 0,
@@ -116,6 +122,7 @@ export default function Step2({
     e.preventDefault();
     console.log(receptientProp);
     console.log(inputs);
+    console.log(receptient);
 
     const recipents: recipent[] = [];
     for (let i = 0; i < inputs.length; i++) {
@@ -128,7 +135,7 @@ export default function Step2({
     }
     console.log(recipents);
     const response = await axios.post(
-      "http://localhost:3000/api/document/addreceptient",
+      "https://ex-sign-swift.vercel.app/api/document/addreceptient",
       { docId, recipient: recipents }
     );
     console.log(response.data, "heell");
@@ -179,61 +186,164 @@ export default function Step2({
   const handleRemoveRecepient = async (id: number) => {
     console.log("recepitn to remove is ", id);
     const response = await axios.delete(
-      `http://localhost:3000/api/document/deleteRecepient/${id}`
+      `https://ex-sign-swift.vercel.app/api/document/deleteRecepient/${id}`
     );
     if (response.data.success) {
       console.log("added succes");
       router.refresh();
     }
   };
-  if (loading3) {
-    return (
-      <div className="w-full h-screen flex items-center justify-center">
-        <Loader />
-      </div>
-    );
-  }
+  // if (loading3) {
+  //   return (
+  //     <div className="w-full h-screen flex items-center justify-center">
+  //       <Loader />
+  //     </div>
+  //   );
+  // }
 
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full  bg-[#F7F7F7]">
       <div className="w-full h-full">
         <form
           id="document-flow-form-container"
-          className=" bg-white sticky flex h-full  flex-col overflow-auto rounded-xl border px-4 py-6"
+          className=" bg-[#F7F7F7] sticky flex h-full  flex-col overflow-auto rounded-xl border px-4 py-6"
         >
           <div className="-mx-2 flex flex-1 flex-col px-2">
-            <h3 className="text-black text-2xl font-semibold">General</h3>
+            <h3 className="text-black text-2xl font-semibold">Add Signers</h3>
             <p className="text-black mt-2 text-sm">
-              Configure general settings for the document.
+              Add the people who will sign the document.
             </p>
-            <div className="border-border mb-0 mt-4"></div>
+            <div className="border  mb-6 mt-4"></div>
             <div className="custom-scrollbar -mx-2 flex flex-1 flex-col overflow-hidden px-2">
               <div className="flex flex-1 flex-col">
                 <div className="space-y-4">
-                  {inputs.map((input) => (
-                    <div key={input.id} className="flex  flex-row gap-2">
-                      <div className="w-full flex flex-row">
-                        <Input
-                          key={input.id}
-                          onChangeName={(e) => handleNameChange(input.id, e)}
-                          onChangeEmail={(e) => handleEmailChange(input.id, e)}
-                          onChangeSignnumber={(e) =>
-                            handleDropDownMenu(input.id, e)
-                          }
-                        />
+                  <div className="flex  flex-row gap-2">
+                    <fieldset className="grid grid-cols-8 gap-4 pb-4">
+                      <div className="space-y-2 relative col-span-3">
+                        <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                          Email
+                          <span className="text-destructive ml-1 inline-block font-medium">
+                            *
+                          </span>
+                        </label>
+                        {emails.map(
+                          (
+                            email: { email: string; name: string },
+                            index: number
+                          ) => (
+                            <input
+                              key={index}
+                              className="bg-background border-input ring-offset-background placeholder:text-muted-foreground/40 focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                              placeholder="Email"
+                              id=":r17b:-form-item"
+                              aria-describedby=":r17b:-form-item-description"
+                              aria-invalid="false"
+                              type="email"
+                              value={email.email}
+                              name={email.name}
+                            />
+                          )
+                        )}
                       </div>
-                      <div className="flex justify-center items-center hover:cursor-pointer"></div>
-                      <div className="flex justify-center items-center hover:cursor-pointer">
-                        <DropdownMenuDemo />
+                      <div className="space-y-2 col-span-3">
+                        <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+                          Name
+                        </label>
+                        {names.map(
+                          (
+                            name: { receptientname: string; name: string },
+                            index: number
+                          ) => (
+                            <input
+                              key={index}
+                              className="bg-background border-input ring-offset-background placeholder:text-muted-foreground/40 focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                              placeholder="Name"
+                              id=":r17c:-form-item"
+                              aria-describedby=":r17c:-form-item-description"
+                              aria-invalid="false"
+                              value={name.receptientname}
+                              name={name.name}
+                            />
+                          )
+                        )}
                       </div>
-                      <div
-                        className="flex justify-center items-center hover:cursor-pointer"
-                        onClick={(e) => handleDelete(e, input.id)}
+                      <div className="space-y-2 col-span-1 mt-auto">
+                        <button
+                          type="button"
+                          role="combobox"
+                          aria-controls="radix-:r17e:"
+                          aria-expanded="false"
+                          aria-autocomplete="none"
+                          dir="ltr"
+                          data-state="closed"
+                          className="border-input ring-offset-background placeholder:text-muted-foreground focus:ring-ring flex h-10 items-center justify-between rounded-md border px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 bg-background w-[60px]"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            className="h-4 w-4"
+                          >
+                            <path d="M12 20h9"></path>
+                            <path d="M16.5 3.5a2.12 2.12 0 0 1 3 3L7 19l-4 1 1-4Z"></path>
+                            <path d="m15 5 3 3"></path>
+                          </svg>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            className="h-4 w-4 opacity-50"
+                            aria-hidden="true"
+                          >
+                            <path d="m6 9 6 6 6-6"></path>
+                          </svg>
+                        </button>
+                        {/* <select aria-hidden="true" name="signers.0.role">
+                            <option value="SIGNER" selected>
+                              Needs to sign
+                            </option>
+                            <option value="APPROVER">Needs to approve</option>
+                            <option value="VIEWER">Needs to view</option>
+                            <option value="CC">Receives copy</option>
+                          </select> */}
+                      </div>
+                      <button
+                        type="button"
+                        className="col-span-1 mt-auto inline-flex h-10 w-10 items-center justify-center text-slate-500 hover:opacity-80 disabled:cursor-not-allowed disabled:opacity-50"
+                        disabled
                       >
-                        <MdDeleteOutline size={25} className=" text-gray-600" />
-                      </div>
-                    </div>
-                  ))}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          className="h-5 w-5"
+                        >
+                          <path d="M3 6h18"></path>
+                          <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
+                          <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
+                        </svg>
+                      </button>
+                    </fieldset>
+                  </div>
+
                   {receptientProp?.result?.map((item: any) => (
                     <div className="flex flex-row gap-2">
                       <div className="flex flex-row gap-2">
@@ -259,7 +369,7 @@ export default function Step2({
                 </div>
                 <div className="mt-4">
                   <button
-                    className="bg-rose-500 hover:bg-rose-700 text-white font-normal py-2 px-4 rounded"
+                    className="bg-rose-500 hover:bg-rose-700 text-white font-normal py-2 px-4 rounded w-2/3"
                     onClick={(e) => onClickHandler(e)}
                   >
                     + Add Signer
